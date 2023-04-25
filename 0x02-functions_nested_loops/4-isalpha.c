160a283c12a3e2c62187ae78c8b7cbb90f533def
Janekam92 <janekam92@gmail.com>
2023-04-25 17:54:52 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ *@c: The character in ASCII code
+ *
+ *Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
