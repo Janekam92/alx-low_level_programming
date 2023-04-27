1d0607ad5921a7474c476a3cd6715143a294baa8
Janekam92 <janekam92@gmail.com>
2023-04-27 18:06:32 +0300

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@
 
 void print_triangle(int size)
 {
-	int j, y, k;
+	int x, y, z;
 
 	if (size <= 0)
 	{
@@ -17,21 +17,18 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (j = 0; j < size; j++)
+		for (x = 0; x < size; x++)
 		{
-			for (y = size - j; y > 1; y--)
-		{
-			_putchar(32);
-		}
-			for (k = 0; k <= j; k++)
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+
+			for (z = 0; z <= x; z++)
 			{
 				_putchar(35);
 			}
 
-			for (k = 0; k <= j; k++)
-			{
-				_putchar(35);
-			}
 			_putchar('\n');
 		}
 	}
